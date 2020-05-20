34b020b6f876a46c8c6ba5deec188cf33f9defdf
Sibusiso <itsbudamain.05@gmail.com>
2020-05-20 22:03:27 +0200

    Implemented tkinter using classes

--- DevEdTut/workflow.py
+++ DevEdTut/workflow.py
@@ -4,7 +4,6 @@ import os
 
 turquoise = '#263d42'
 
-root = tk.Tk()
 apps = []
 
 if os.path.isfile('save.txt'):
@@ -16,40 +15,57 @@ if os.path.isfile('save.txt'):
 		apps = [app for app in tempApps if app.strip()]
 		print(apps)
 
-def addApp():
-	for widget in frame.winfo_children():
-		widget.destroy()
+class App(tk.Tk):
+	def __init__(self):
+		self = super().__init__()
+
+	def create_widgets():
+		canvas = tk.Canvas(root, bg=turquoise)
+		canvas.pack(fill='both')
+
+		frame = tk.Frame(root, bg='white')
+		frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+
+		openFile = tk.Button(root, text='Open file', padx=10, pady=5, fg='white', bg=turquoise, command = addApp)
+		openFile.pack(side='left')
+
+		runApps = tk.Button(root, text='Run Apps', padx=10, pady=5, fg='white', bg=turquoise, command = runApps)
+		runApps.pack(side='left')
+
+	def addApp():
+		for widget in frame.winfo_children():
+			widget.destroy()
+
+			filename = filedialog.askopenfilename(initialdir='/', title='Select FIle', filetypes=(('executables','*.exe'), ('all files', '*.*')))
+			apps.append(filename)
+			print(filename)
+			for app in apps:
+				label = tk.Label(frame, text=app, bg='gray',pady=2)
+				label.pack()
+
+		def runApps():
+			for app in apps:
+				os.startfile(app)		
+
+	def run(self):
+		self.mainloop()
+
+	def refresh(self):
+		for app in apps:
+			label = tk.Label(frame, text=app)
+			label.pack(side='top', fill='x',ipady=10,pady=1,padx=1)
 
 
-	filename = filedialog.askopenfilename(initialdir='/', title='Select FIle', filetypes=(('executables','*.exe'), ('all files', '*.*')))
-	apps.append(filename)
-	print(filename)
-	for app in apps:
-		label = tk.Label(frame, text=app, bg='gray')
-		label.pack()
+def main():
+	app = App()
+	app.title('workflows')
+	app.state('zoomed')
+	app.run()
 
-def runApps():
-	for app in apps:
-		os.startfile(app)
+	with open('save.txt', 'w') as f:
+		for app in apps:
+			f.write(app + ',')
 
-canvas = tk.Canvas(root, height=600, width=600, bg=turquoise)
-canvas.pack()
+if __name__ == '__main__':
+	main()
 
-frame = tk.Frame(root, bg='white')
-frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
-
-openFile = tk.Button(root, text='Open file', padx=10, pady=5, fg='white', bg=turquoise, command = addApp)
-openFile.pack()
-
-runApps = tk.Button(root, text='Run Apps', padx=10, pady=5, fg='white', bg=turquoise, command = runApps)
-runApps.pack()
-
-for app in apps:
-	label = tk.Label(frame, text=app)
-	label.pack()
-
-root.mainloop()
-
-with open('save.txt', 'w') as f:
-	for app in apps:
-		f.write(app + ',')
